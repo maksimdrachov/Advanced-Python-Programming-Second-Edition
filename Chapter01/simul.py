ada92da07603348bf563970b475e5e6c82986e8c
Maksim Drachov <maksim.drachov@outlook.com>
2022-12-23 13:20:48 +0200

    ch-1

--- Chapter01/simul.py
+++ Chapter01/simul.py
@@ -18,33 +18,34 @@ class ParticleSimulator:
     def __init__(self, particles):
         self.particles = particles
 
+    # @profile
+    # def evolve(self, dt):
+    #     timestep = 0.00001
+    #     nsteps = int(dt / timestep)
+
+    #     for i in range(nsteps):
+    #         for p in self.particles:
+
+    #             norm = (p.x**2 + p.y**2) ** 0.5
+    #             v_x = (-p.y) / norm
+    #             v_y = p.x / norm
+
+    #             d_x = timestep * p.ang_speed * v_x
+    #             d_y = timestep * p.ang_speed * v_y
+
+    #             p.x += d_x
+    #             p.y += d_y
+
     def evolve(self, dt):
         timestep = 0.00001
         nsteps = int(dt / timestep)
 
-        for i in range(nsteps):
-            for p in self.particles:
-
-                norm = (p.x ** 2 + p.y ** 2) ** 0.5
-                v_x = (-p.y) / norm
-                v_y = p.x / norm
-
-                d_x = timestep * p.ang_speed * v_x
-                d_y = timestep * p.ang_speed * v_y
-
-                p.x += d_x
-                p.y += d_y
-
-    # def evolve(self, dt):
-    #     timestep = 0.00001
-    #     nsteps = int(dt/timestep)
-
-    #     # First, change the loop order
-    #     for p in self.particles:
-    #         t_x_ang = timestep * p.ang_speed
-    #         for i in range(nsteps):
-    #             norm = (p.x**2 + p.y**2)**0.5
-    #             p.x, p.y = p.x - t_x_ang*p.y/norm, p.y + t_x_ang * p.x/norm
+        # First, change the loop order
+        for p in self.particles:
+            t_x_ang = timestep * p.ang_speed
+            for i in range(nsteps):
+                norm = (p.x**2 + p.y**2) ** 0.5
+                p.x, p.y = p.x - t_x_ang * p.y / norm, p.y + t_x_ang * p.x / norm
 
 
 def visualize(simulator):
