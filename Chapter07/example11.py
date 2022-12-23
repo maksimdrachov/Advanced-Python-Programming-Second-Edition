649b38080a9f832b61e48cefaed854546085ceb2
Maksim Drachov <maksim.drachov@outlook.com>
2022-12-23 17:06:07 +0200

    ch-7

--- Chapter07/example11.py
+++ Chapter07/example11.py
@@ -3,7 +3,7 @@ import asyncio
 
 async def network_request(number):
     await asyncio.sleep(1.0)
-    return {"success": True, "result": number ** 2}
+    return {"success": True, "result": number**2}
 
 
 async def fetch_square(number):
@@ -16,13 +16,13 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
     ### First example
-    loop.run_until_complete(fetch_square(2))
-    loop.run_until_complete(fetch_square(3))
-    loop.run_until_complete(fetch_square(4))
+    # loop.run_until_complete(fetch_square(2))
+    # loop.run_until_complete(fetch_square(3))
+    # loop.run_until_complete(fetch_square(4))
 
     ### Second example
-    # asyncio.ensure_future(fetch_square(2))
-    # asyncio.ensure_future(fetch_square(3))
-    # asyncio.ensure_future(fetch_square(4))
-    # # Hit Ctrl-C to stop the loop
-    # loop.run_forever()
+    asyncio.ensure_future(fetch_square(2))
+    asyncio.ensure_future(fetch_square(3))
+    asyncio.ensure_future(fetch_square(4))
+    # Hit Ctrl-C to stop the loop
+    loop.run_forever()
